45ba33448519ba34b0576c95f0fec932a4673d86
capittan <113373149+capittan@users.noreply.github.com>
2023-04-16 17:46:40 +0300

    Added activity AddNewCategory

--- app/src/main/java/com/example/android_store/catalog/AddNewCategory.java
+++ app/src/main/java/com/example/android_store/catalog/AddNewCategory.java
@@ -1,23 +1,23 @@
-package com.example.android_store;
+package com.example.android_store.catalog;
 
-import android.content.Intent;
+
+
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.android_store.R;
 import com.example.android_store.application.HomeApplication;
-import com.example.android_store.catalog.AddNewCategory;
 
-public class MainActivity extends AppCompatActivity {
+public class AddNewCategory extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_new_category);
 
         ImageView imgLogo = (ImageView) findViewById(R.id.imgLogo);
         String url = "https://www.collinsdictionary.com/images/full/tree_267376982.jpg";
@@ -25,10 +25,5 @@ public class MainActivity extends AppCompatActivity {
                 .load(url)
                 .apply(new RequestOptions().override(600))
                 .into(imgLogo);
-    }
-
-    public void onClickBtn(View view) {
-        Intent intent = new Intent(MainActivity.this, AddNewCategory.class);
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/android_store/catalog/CatalogActivity.java
+++ app/src/main/java/com/example/android_store/catalog/CatalogActivity.java
@@ -1,11 +1,11 @@
 package com.example.android_store.catalog;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
 
 import com.example.android_store.R;
 import com.example.android_store.catalog.categoryCard.CategoryAdapter;
@@ -19,7 +19,6 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class CatalogActivity extends AppCompatActivity {
-
     CategoryAdapter categoryAdapter;
     private RecyclerView rcvCategories;
 
